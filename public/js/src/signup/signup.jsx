8cab92698b0cb795176704b524d7f9d6ee13d944
Kevin Tan <kevin@ktizzel.com>
2016-01-22 01:31:30 -0500

    description

--- public/js/src/signup/signup.jsx
+++ public/js/src/signup/signup.jsx
@@ -82,14 +82,29 @@ var Container = React.createClass({
     }
     return (
       <div className="Container">
-        <div className="RoadBackground">
-          <img src="/images/yale.png"/>
-        </div>
-        <Title/>
-        <form className="InputBox" onSubmit={this.handleSubmit}>
-          <input style={style} ref="email" type="text" placeholder="Signup With Your Yale Email"/>
-          {Message}
-        </form>
+        <section className="Slide1">
+          <div className="RoadBackground">
+            <img src="/images/yale.png"/>
+          </div>
+          <Title/>
+          <form className="InputBox" onSubmit={this.handleSubmit}>
+            <input style={style} ref="email" type="text" placeholder="Signup With Your Yale Email"/>
+            {Message}
+          </form>
+        </section>
+        <section className="Slide2">
+          <p className="Description1">
+            We believe the more visible a path, the more people are likely to take a chance and travel it. Our mission is to utilize the resources of Yale and Calhoun College to illuminate diverse, unconventional and worthwhile paths.
+          </p>
+          <p className="Description2">
+            To begin, we will have a once-a-month luncheon where we invite up to five people from different industries and walks of life to Calhoun. Our guests will talk about their respective careers and fields: why they chose them, how they got there, and how they can help students get there. This won't be a long presentation where students just passively listen. Instead, the event will be set up as multiple concurrent conversations at tables in the dining hall. You can sit with the guests and have a conversation.
+           </p>
+          <p className="Description3">
+            We will also have a Slack dedicated to TraveledBy, where students and speakers can connect, Q&A, and share opportunities and info over the web. Add your email above and we’ll send you an invite!
+            <br/><br/>
+            keep searching, never settle
+          </p>
+       </section>
       </div>
     )
   }
